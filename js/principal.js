bfce92758246a49c1c768c73279c733ca8b8e945
Susan Navarro <navarrolalupu7@gmail.com>
2024-10-13 20:13:12 -0500

    Implementacion logout examen

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,10 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
 
     // referenciar controles de pantalla
     const msgSuccess = this.document.getElementById('msgSuccess');
-
+    const msgError = this.document.getElementById('msgError');
+    
+    
     // recuperar nombre de usuario
     const result = JSON.parse(this.localStorage.getItem('result'));
 
@@ -14,4 +16,45 @@ window.addEventListener('load', function(){
 function mostrarAlerta(mensaje) {
     msgSuccess.innerHTML = mensaje;
     msgSuccess.style.display = 'block';
+}
+function cerrarAlertSuccess(mensaje){
+    msgSuccess.innerHTML = mensaje;
+    msgSuccess.style.display = 'none';
+}
+const mostrarAlertError = (mensaje) => {
+    msgError.innerHTML = mensaje;
+    msgError.style.display = 'block';
+}
+
+const idButtonLogout = document.getElementById('logout');
+
+idButtonLogout.addEventListener('click', () => {
+    const userAutenticado = localStorage.getItem('result');
+    if (!userAutenticado) return;
+    logout();
+});
+
+const logout = async () => {
+    const url = 'http://localhost:8082/login/logout';
+    const userAutenticado = JSON.parse(localStorage.getItem('result'));
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: userAutenticado.nombreUsuario
+        });
+        console.log("RESPONSE STATUS: ", response.status);
+        if (response.status === 200) {
+            localStorage.clear();
+            window.location.replace('index.html');
+        } else {
+            cerrarAlertSuccess()
+            mostrarAlertError('Error: Ocurrio un de tiempo')
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+    }
 }
